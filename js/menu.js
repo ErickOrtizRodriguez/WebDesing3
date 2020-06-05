50013850e2a9d7f1745ffe25d1d409e6d7a5730e
daguito93-2020 <66398313+daguito93-2020@users.noreply.github.com>
2020-06-04 23:48:11 -0600

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let btnmenu = document.getElementById('botonmenu');
+let menu = document.getElementById('menu');
+
+btnmenu.addEventListener('click', function(){
+  'use strict';
+  menu.classList.toggle('mostrar');
+});
