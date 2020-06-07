cf06d511c9aee5301d114be843ab92e80053db98
daguito93-2020 <66398313+daguito93-2020@users.noreply.github.com>
2020-06-06 20:12:27 -0600

    Delete menu.js

--- webDesing3/js/menu.js
+++
@@ -1,7 +0,0 @@
-let btnmenu = document.getElementById('botonmenu');
-let menu = document.getElementById('menu');
-
-btnmenu.addEventListener('click', function(){
-  'use strict';
-  menu.classList.toggle('mostrar');
-});
